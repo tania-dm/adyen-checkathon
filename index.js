35065f71480e69ce4610c515eacd6d3a8ea525b8
Deeksha Sharma <deeksha.s@nl.abnamro.com>
2019-02-09 17:55:38 +0100

    updated start script

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+var path = require('path');
+app.get('*', function(req, res) {
+    res.sendfile(path.resolve('views/index.html'));
+});
 app.listen(port, () => {
   console.log(`Express server listening on port ${port}`);
 })
+
